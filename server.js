c6556809a061c580f8b3bda01da9ad0c095c61b0
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-03-27 15:51:37 -0400

    Moved frontend code to public director. Added server.js and modified package.json to serve web app using express.

---
+++ server.js
@@ -0,0 +1,19 @@
+/* This is the server code */
+
+var http = require('http');
+var fs = require('fs');
+var express = require('express');
+
+var app = express();
+var router = express.Router();
+
+const PORT=80; 
+
+/* Load the homepage */
+app.use(express.static('public'));
+
+
+app.listen(PORT, function() {
+    console.log('Example app listening on port 3000!');
+  });
+  
